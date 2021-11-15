8a8f1afc98535a1d6cb341cb3be682a8675dbce5
Samara-rbd <maiareismaia@gmail.com>
2021-11-15 11:31:57 -0300

    tentando subir

--- src/routes.ts
+++ src/routes.ts
@@ -6,6 +6,10 @@ import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
 const router = Router();
 
 router.post("/authenticate", new AuthenticateUserController().handle);
-router.post("/messages", ensureAuthenticated, new CreateMessageController().handle);
+router.post(
+    "/messages",
+     ensureAuthenticated, 
+     new CreateMessageController().handle
+);
 
-export { router }
+export { router };
